4ee0a38dca2fe7e864eb54471ff495b885563a52
rrodriguezldam1 <rrodriguezldam1@gmail.com>
2020-10-31 12:20:33 +0100

    Procesado de archivo JSON y Primera panatalla (ListaPuntos Screen)

---
+++ lib/providers/puntos_provider.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+import '../models/puntoalojamientos_models.dart';
+import '../models/puntos_model.dart';
+
+
+class PuntosProvider {
+ final listaTipos = [
+   'Cambiar Nombre',
+ ];
+
+ List<PuntoAlojamiento> listaPuntos = [];
+
+ Future<List<PuntoAlojamiento>> cargarPuntos() async{
+   final data = await rootBundle.loadString('assets/data/Opendata_Resultados_DD_es.json');
+   final decodedData = json.decode(data);
+   final openData = decodedData['OpenData'];
+   final openDataRow = openData['OpenDataRow'];
+   Puntos puntos = Puntos.fromJsonList(openDataRow);
+   listaPuntos = puntos.lista;
+   return listaPuntos;
+  }
+}
+
+final puntosProvider = new PuntosProvider();
---
+++ lib/screens/listpuntos_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../models/puntoalojamientos_models.dart';
+import '../providers/puntos_provider.dart';
+
+class ListaPuntosScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('ListaPuntos Screen'),),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+    return FutureBuilder(
+      future: puntosProvider.cargarPuntos(),
+      initialData: [],
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ListView(
+            children: _listaElementos(snapshot.data),
+          );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
+  List<Widget> _listaElementos(List<PuntoAlojamiento> data) {
+    final List<Widget> lst = [];
+    data.forEach((al) {
+      final w = ListTile(
+        title: Text(al.localidad),
+        subtitle: Text(al.direccion),
+      );
+      lst.add(w);
+      lst.add(Divider());
+     });
+     return lst;
+  }
+}
