af8b13f5b75fa847758522e18e82fce35c892610
ionene <ione8ibanez@gmail.com>
2020-11-01 13:55:56 +0100

    lista modalidades

--- lib/providers/puntos_provider.dart
+++ lib/providers/puntos_provider.dart
@@ -5,22 +5,22 @@ import 'package:flutter/services.dart';
 import '../models/puntoalojamientos_models.dart';
 import '../models/puntos_model.dart';
 
-
 class PuntosProvider {
- final listaTipos = [
-   'Cambiar Nombre',
- ];
+  final listaTipos = [
+    'Cambiar Nombre',
+  ];
 
- List<PuntoAlojamiento> listaPuntos = [];
+  List<PuntoAlojamiento> listaPuntos = [];
 
- Future<List<PuntoAlojamiento>> cargarPuntos() async{
-   final data = await rootBundle.loadString('assets/data/Opendata_Resultados_DD_es.json');
-   final decodedData = json.decode(data);
-   final openData = decodedData['OpenData'];
-   final openDataRow = openData['OpenDataRow'];
-   Puntos puntos = Puntos.fromJsonList(openDataRow);
-   listaPuntos = puntos.lista;
-   return listaPuntos;
+  Future<List<PuntoAlojamiento>> cargarPuntos() async {
+    final data = await rootBundle
+        .loadString('assets/data/Opendata_Resultados_DD_es.json');
+    final decodedData = json.decode(data);
+    final openData = decodedData['OpenData'];
+    final openDataRow = openData['OpenDataRow'];
+    Puntos puntos = Puntos.fromJsonList(openDataRow);
+    listaPuntos = puntos.lista;
+    return listaPuntos;
   }
 }
 
